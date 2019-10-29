3e46528bdf8c90fc17a6504c5f587c83029df17c
Nolan Kohler <kohlern@wlhs.wlwv.k12.or.us>
2019-10-29 09:55:07 -0700

    Flip coin 10 times say number of heads and tails

---
+++ CoinTester.java
@@ -0,0 +1,13 @@
+//creates instances of coin class and tests attributes and behaviors
+
+public class CoinTester{
+  public static void main(String[] args) {
+    Coin myCoin = new Coin();
+    Coin yourCoin = new Coin();
+
+    for (int i = 1; i <= 10; i++){
+    System.out.println("My coin = " + myCoin.toString());
+}
+
+  }
+}
